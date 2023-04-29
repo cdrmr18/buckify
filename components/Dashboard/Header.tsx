49c998d0582d09f4ff37efd5352bf39a4341e28b
Kathy Tavia <kathy.tavia@gmail.com>
2023-04-29 22:52:37 +0200

    style mobile view

--- components/Dashboard/Header.tsx
+++ components/Dashboard/Header.tsx
@@ -2,20 +2,24 @@
 
 const Header = () => {
   return (
-    <div className="border-b py-8 w-full mx-auto px-6 lg:max-w-6xl lg:px-8">
-      <h1 className="text-lg font-semibold my-4">Dashboard</h1>
-      <div className="flex flex-col md:flex-row mx-auto max-w-6xl space-y-4">
+    <div className="bg-gray-100 border-b py-8 w-full mx-auto px-6 lg:max-w-6xl lg:px-8">
+      <h1 className="text-xl font-semibold my-6">Dashboard</h1>
+      <div className="flex flex-col md:flex-row mx-auto max-w-6xl space-y-6">
         <div className="basis-1/2 flex-col space-y-5 md:flex md:items-start md:justify-around lg:border-gray-200">
           {/* FInance */}
           <div>
-            <h2 className="mb-6 font-bold text-3xl">$50,000</h2>
-            <div className="flex justify-between text-2xl">
-              <p>$75,000</p>
-              <p>Savings Goal</p>
-            </div>
-            <div className="mt-3 flex justify-between text-2xl">
-              <p>$25,000</p>
-              <p>Amount needed</p>
+            <h2 className="mb-6 font-bold text-4xl">$50,000</h2>
+
+            <div className="flex justify-between pr-32">
+              <div className="text-xl space-y-2">
+                <p className="font-light">Savings Goal</p>
+                <p className="font-light">Amount needed</p>
+              </div>
+
+              <div className="text-xl font-semibold space-y-2">
+                <p>$75,000</p>
+                <p >$25,000</p>
+              </div>
             </div>
           </div>
 
@@ -37,7 +41,23 @@ const Header = () => {
         </div>
 
         {/* Statistics */}
-        <div className="basis-1/2">Statistics</div>
+        <div className="basis-1/2 space-y-3">
+          <div className="bg-blue-100 border border-gray-800 rounded-sm shadow-sm px-4 py-3 flex">
+            <div className="basis-1/2">
+              <p className="text-xl font-bold">$30,000</p>
+              <p className="text-sm font-semibold">Total savings</p>
+            </div>
+            <div className="basis-1/2">charts</div>
+          </div>
+
+          <div className="bg-yellow-100 border border-gray-800 rounded-sm shadow-sm px-4 py-3 flex">
+            <div className="basis-1/2">
+              <p className="text-xl font-bold">$30,000</p>
+              <p className="text-sm font-semibold">Total savings</p>
+            </div>
+            <div className="basis-1/2">charts</div>
+          </div>
+        </div>
       </div>
     </div>
   );
